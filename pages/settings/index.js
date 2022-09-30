3764c56d853e345432acc6259f8bb43412b5b857
Jorge Colmenero <jorge.colm23@gmail.com>
2022-09-30 00:40:10 -0500

    testing environmental variables

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -12,6 +12,7 @@ export default function settings(){
     return (
         <div>
             <h1>Not sure what settings I want to include here to be honest ...</h1>
+            <p>checking my giggles, {process.env.NEXT_PUBLIC_word_1} and {process.env.word_2}</p>
         </div>
     )
 }
