78f2432d557fa90ec2438eb8bb562d400dbaa57a
Jorge Colmenero <jorge.colm23@gmail.com>
2022-10-21 15:00:52 -0500

    fixing hidden api calls

--- pages/api/orders/getTableDetails.js
+++ pages/api/orders/getTableDetails.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     const {word1, word2} = req.query;
     
     if (req.method === 'GET') {
-        const newRes = await fetch(`${process.env.ordersEndpoint}?${process.env.inventorySecretWord1}=${word1}&${process.env.inventorySecretWord2}=${word2}`);
+        const newRes = await fetch(`${process.env.ordersEndpoint}?${process.env.ordersSecretWord1}=${word1}&${process.env.ordersSecretWord2}=${word2}`);
         const data = await newRes.json();
         // console.log('word1',word1, 'word2', word2);
         res.status(201).json(data);
