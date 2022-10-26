7423883bf0e05bab6cc47ddc4c0e8a1236692f59
Jorge Colmenero <jorge.colm23@gmail.com>
2022-10-26 13:54:49 -0500

    added CSSTransition to search bar placeholder, along with cubic bezier function to mimic delay

--- components/SearchBar/SearchBar.jsx
+++ components/SearchBar/SearchBar.jsx
@@ -3,6 +3,8 @@ import {FormProvider, useForm} from 'react-hook-form';
 import {ErrorMessage} from '@hookform/error-message';
 import {useState} from 'react';
 import LoadingSpinner from '../forms/customFormTransitions/LoadingSpinner';
+import { CSSTransition } from 'react-transition-group';
+import SearchBarPlaceholderTransitions from './SearchBarPlaceholderTransitions.module.scss';
 
 
 export default function SearchBar({category, fieldsToSearch}){
@@ -38,15 +40,51 @@ export default function SearchBar({category, fieldsToSearch}){
     }
 
     const watchSearch = watch("wordToSearch", '');
+    const isDirty = () => {
+        if (watchSearch != '') {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    
+    const [isFocused, setIsFocused] = useState(false);
+    const changeFocus = () => {
+        // console.log('is focused, before set call=', isFocused)
+        // console.log('precheck of focused state', !isFocused)
+        setIsFocused(true);
+        // console.log('a focus has occured....')
+        // console.log('is focused, after set call =', isFocused)
+    }
+    const blurFocus = () => {
+        // console.log('blurring, before call', isFocused)
+        setIsFocused(false);
+        // console.log('blurring has occured', isFocused)
+    }
+
+    // onFocus={() => changeFocus()}
+    // onBlur = {() => blurFocus()}
+
     return (
         <div className={styles.searchBarContainer}>
             <form onSubmit={handleSubmit(submitSearch)} className={styles.searchBarForm}>
                 <label htmlFor={category} className={styles.searchBarContent}> 
                     <input {...register("wordToSearch", {required:true})}  type='text'
-                        required className={styles.searchBarInputField}/>
-                    <span className = {styles.searchBarPlaceholder}>Enter {spanText}</span>
+                        required className={styles.searchBarInputField}
+                        onClick={()=>setIsFocused(!isFocused)} autocomplete="off"/>
+                    <CSSTransition 
+                        in = {isFocused}
+                        classNames = {SearchBarPlaceholderTransitions}
+                        timeout={1000}
+                        onExited = {()=>{console.log('exiting animation', isFocused)}}
+                        onEnter = {()=>{console.log('entering animation', isFocused)}}
+                    >
+                        <div className={styles.searchBarPlaceholder}>
+                            <span>Enter {spanText}</span>
+                        </div>
+                    </CSSTransition>
+                    
+                    
                 </label>
                 <button type='submit' className={styles.searchButton} type="submit"> Search {category} </button>
             </form>
