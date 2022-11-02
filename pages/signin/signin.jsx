4b260676a85c5126a606743a2e77f15005417ec5
Jorge Colmenero <jorge.colm23@gmail.com>
2022-11-02 00:06:32 -0500

    added react hook form with yup validation to the signin page

--- pages/signin/signin.jsx
+++ pages/signin/signin.jsx
@@ -1,21 +1,51 @@
 import styles from './signin.module.scss';
-import {signIn} from 'next-auth/react';
+import {signIn, getCsrfToken, getProviders} from 'next-auth/react';
 import Image from 'next/image';
 import ravisLogo from '../../public/ravis_logo.png';
 import { CSSTransition } from 'react-transition-group';
 import titleTransitionsRight from './titleTransitionsRight.module.scss';
 import titleTransitionsLeft from './titleTransitionsLeft.module.scss';
 import {useState, useEffect} from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
 
+export async function getServerSideProps(context) {
+    return {
+        props: {
+            csrfToken: await getCsrfToken(context),
+            providers: await getProviders(),
+        },
+    };
+}
 
-export default function SignIn(){
+export default function SignIn({csrfToken, providers}){
 
     const [signInFocus, setSignInFocus] = useState(false);
+    // const [error, setError] = useState(null);
 
     useEffect(() => {
         setSignInFocus(true);
     });
 
+
+    // react hook form and yup initialization
+    const schema = yup.object().shape({
+        username: yup.string().required('An email is required.'),
+        password: yup.string().required('A password is required to log in.'),
+    });
+
+    const {register, handleSubmit, watch, formState:{errors, isSubmitting}} = useForm({
+        resolver: yupResolver(schema)
+    });
+
+    // const watchInputs = watch(["username", "password"]);
+    // console.log(watchInputs);
+
+
+    const onSubmit =  (data, e) => {
+        e.target.submit();
+    };
 
     return (
         <div className = {styles.mainBackground}>
@@ -27,7 +57,6 @@ export default function SignIn(){
                     height={192}
                 />
                 <div className={styles.rectangleContainerLeft}>
-                    
                     <CSSTransition 
                         in = {signInFocus}
                         classNames = {titleTransitionsLeft}
@@ -37,13 +66,10 @@ export default function SignIn(){
                     >
                         <div className ={styles.rectangleLeft}>Management</div>
                     </CSSTransition>
-                
-
                 </div>
             </div>
             <div className= {styles.rightBackground}>
                 <div className={styles.rectangleContainerRight}>
-
                     <CSSTransition 
                         in = {signInFocus}
                         classNames = {titleTransitionsRight}
@@ -53,32 +79,31 @@ export default function SignIn(){
                     >
                         <div className ={styles.rectangleRight}>Management</div>
                     </CSSTransition>
-                
-
                 </div>
                 <div className={styles.fixedTitleContainer}>
                     <div className = {styles.fixedTitle}>
                         Dashboard
                     </div>
                 </div>
-                <div className={styles.signinFormContainer}>
+                <form action={providers['credentials'].callbackUrl} method="post" onSubmit={handleSubmit(onSubmit)} className={styles.signinFormContainer}>
                     <div className={styles.signinFormField}>
                         <label className={styles.signinFormFieldLabel}>
                             Username:
                         </label>
-                        <input type='text' autoComplete='off' placeholder='' 
+                        <input name = 'username' {...register('username')} autoComplete='off' placeholder='' 
                             className={styles.inputStyle}></input>
                     </div>
                     <div className={styles.signinFormField}>
                         <label className={styles.signinFormFieldLabel}>
                             Password:
                         </label>
-                        <input type='text' autoComplete='off' placeholder='' className={styles.inputStyle}></input>
+                        <input name = 'password' {...register('password')} autoComplete='off' placeholder='' className={styles.inputStyle}></input>
                     </div>
                     <div className={styles.signinButtonContainer}>
-                        <button className={styles.signInButton} onClick={()=>signIn()}>Sign In</button>
+                        <button disabled = {isSubmitting} className={styles.signInButton} type='submit'>Sign In</button>
                     </div>
-                </div>
+                    <input name="csrfToken" type="hidden" value={csrfToken} />
+                </form>
                 
             </div>
 
