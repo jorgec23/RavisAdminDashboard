4bb484dfeca0004145b286568c5a61227551a18d
Jorge Colmenero <jorge.colm23@gmail.com>
2022-11-03 14:29:20 -0500

    removing config never redirected back to homepage so added all page routes to config, including home

--- middleware.js
+++ middleware.js
@@ -1,8 +1,10 @@
 export { default } from "next-auth/middleware";
 
-// export const config = {matcher: [
-//     "/orders/:path*",
-//     "/products/:path*",
-//     "/settings/:path*",
-//     "/users/:path*",
-// ]}
+export const config = {matcher: [
+    // "/:path*",
+    "/orders/:path*",
+    "/products/:path*",
+    "/settings/:path*",
+    "/users/:path*",
+    "/",
+]}
