8a930599f7b4d8c33e4b5dd470f79d58f346cfae
Jorge Colmenero <jorge.colm23@gmail.com>
2022-11-08 02:58:17 -0600

    testing transitions without the absolute positioning

--- components/forms/applications/ApplicationFormInput.jsx
+++ components/forms/applications/ApplicationFormInput.jsx
@@ -1,16 +1,33 @@
 import styles from "./ApplicationFormInput.module.scss";
+import { CSSTransition } from 'react-transition-group';
+import downTransitions from './customTransitions/down.module.scss';
+import {useState, useEffect} from 'react';
 
 
 export default function ApplicationFormInput(fieldName, fieldValue, backgroundColor, span) {
 //     console.log(backgroundColor);
+
+    const [mounted, setMounted] = useState(false);
+    useEffect(() =>{
+        setMounted(true);
+    });
+
     return (
         <div className={styles.mainContainerWrapper}> 
-            <div className = {styles.mainContainer} style = {{backgroundColor: backgroundColor, gridColumn: span}}>
-                <div className={styles.fieldNameContainer}>{fieldName}</div>
-                <div className={styles.fieldValueContainer}>
-                    <div className={styles.fieldValuePlaceholder}>{fieldValue}</div>
+            <CSSTransition
+                in={mounted}
+                classNames={downTransitions}
+                timout={500}
+                appear={true}
+            >
+                <div className = {styles.mainContainer} style = {{backgroundColor: backgroundColor, gridColumn: span}}>
+                    <div className={styles.fieldNameContainer}>{fieldName}</div>
+                    <div className={styles.fieldValueContainer}>
+                        <div className={styles.fieldValuePlaceholder}>{fieldValue}</div>
+                    </div>
                 </div>
-            </div>
+            </CSSTransition>
+            
         </div>
         
     )
