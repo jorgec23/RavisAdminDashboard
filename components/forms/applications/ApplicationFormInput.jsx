30204a88d86a46beba942dfabb88b2c112e9dd1c
Jorge Colmenero <jorge.colm23@gmail.com>
2022-11-08 02:20:54 -0600

    testing a position relative wrapper around the application field input component with position absolute, to allow for smooth translation animations

--- components/forms/applications/ApplicationFormInput.jsx
+++ components/forms/applications/ApplicationFormInput.jsx
@@ -4,11 +4,14 @@ import styles from "./ApplicationFormInput.module.scss";
 export default function ApplicationFormInput(fieldName, fieldValue, backgroundColor, span) {
 //     console.log(backgroundColor);
     return (
-        <div className = {styles.mainContainer} style = {{backgroundColor: backgroundColor, gridColumn: span}}>
-            <div className={styles.fieldNameContainer}>{fieldName}</div>
-            <div className={styles.fieldValueContainer}>
-                <div className={styles.fieldValuePlaceholder}>{fieldValue}</div>
+        <div className={styles.mainContainerWrapper}> 
+            <div className = {styles.mainContainer} style = {{backgroundColor: backgroundColor, gridColumn: span}}>
+                <div className={styles.fieldNameContainer}>{fieldName}</div>
+                <div className={styles.fieldValueContainer}>
+                    <div className={styles.fieldValuePlaceholder}>{fieldValue}</div>
+                </div>
             </div>
         </div>
+        
     )
 }
