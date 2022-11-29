7ad8f8cc64e1cfbf5822cabbfe9cfa028cd84765
Jorge Colmenero <jorgecolmenero@Jorges-MBP.askey.com>
2022-11-29 03:02:08 -0600

    expanded size of chart by styling svg directly

--- components/Graphs/BarChart2.jsx
+++ components/Graphs/BarChart2.jsx
@@ -16,7 +16,8 @@ export default function Chart() {
 
     useEffect(()=> {
         const margin = {top: 50, right:30, bottom:30, left:60}
-        console.log(d3.select(d3Chart));
+        console.log(d3.select(d3Chart.current).style('width'));
+        console.log(d3.select(d3Chart.current).style('height'));
 
         const chartWidth = parseInt(d3.select(d3Chart.current).style('width')) - margin.left - margin.right;
         const chartHeight = parseInt(d3.select(d3Chart.current).style('height')) - margin.left - margin.right; 
@@ -33,11 +34,32 @@ export default function Chart() {
         svg.append('g')
             .attr('transform', 'translate(0,'+ chartHeight + ')')
             .call(d3.axisBottom(x).tickFormat(i=>sampleData[i].category).tickSizeOuter(0))
+
+        const max = d3.max(sampleData, function(d){return d.quantity})
+
+        const y = d3.scaleLinear()
+                    .domain([0, max])
+                    .range([chartHeight, margin.top]);
+
+        svg.append('g')
+            .attr('transform', 'translate(' + margin.left + ',0)')
+            .call(d3.axisLeft(y));
+
+        svg.append('g')
+            .attr('fill', '#65f0eb')
+            .selectAll('rect')
+            .data(sampleData)
+            .join('rect')
+                .attr('x', (d,i) => x(i))
+                .attr('y', d=> y(d.quantity))
+                .attr('height', d=>y(0)-y(d.quantity))
+                .attr('width', x.bandwidth())
+
     },[])
 
     return (
         <div id={styles["d3demo"]}>
-            <svg ref={d3Chart}></svg>
+            <svg ref={d3Chart} className={styles.chart2Size}></svg>
         </div>
     )
 }
